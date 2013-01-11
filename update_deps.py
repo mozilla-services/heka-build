1d4ad0cb188e39dd2e7b90ec319ca7fff22abb5e
Ben Bangert <ben@groovie.org>
2013-01-10 18:28:59 -0800

    Add more of the doc string

--- update_deps.py
+++ update_deps.py
@@ -1,1 +1,8 @@
-"""Script that reads a file to update package dependencies"""
+"""Script that reads a file to update package dependencies
+
+This script reads a file formatted to indicate the project, whether the
+project should be checked out, or fetched with go get, and the specific
+changeset number.
+
+
+"""
