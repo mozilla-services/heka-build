70d8e6212c7d6f4a5c353d08402e1c254af0d226
Ben Bangert <ben@groovie.org>
2013-01-14 09:08:47 -0800

    Bit more progress on update reps.

--- update_deps.py
+++ update_deps.py
@@ -4,5 +4,49 @@ This script reads a file formatted to indicate the project, whether the
 project should be checked out, or fetched with go get, and the specific
 changeset number.
 
+Example package file::
+
+    github.com/bitly/go-simplejson 477b2e70b12d
+
+Two fields are present in a single line:
+
+    repository checkout_flag changeset_hash
+
+The changeset_hash can be left off, in which case this will always
+attempt to update the code every time its run. The checkout_flag must
+be either a 0 or 1 to indicate if the repo should be checked out.
+
+Checking out repositories that are sub-directories is not supported.
+
+To run this script, designate a file to read. The Go workspace to
+operate in should be set in the environ as GOPATH.
+
+Example::
+
+    python update_deps.py package_deps.txt
 
 """
+from __future__ import print_function
+import os
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("No configuration file specified.")
+        return
+    gopath = os.environ.get('GOPATH')
+    if not gopath:
+        print("No GOPATH environment variable has been set.")
+        return
+    conf_file = sys.argv[1]
+    if not os.path.exists(conf_file):
+        print("Config file not found:", conf_file)
+        return
+
+    with open(conf_file, 'r') as f:
+        package_lines = [x.strip() for x in f.readlines() if x.strip()]
+
+    print(package_lines)
+
+main()
