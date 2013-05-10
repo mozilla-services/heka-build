00b1816361d2ca077c63165274c8a7d5d0a1cf56
Michael Trinkala <trink@acm.org>
2013-05-09 17:07:05 -0700

    Move the Heka respository clone into the batch file

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 set mozilla_path=src\github.com\mozilla-services
+set git="c:\Program Files (x86)\Git\bin\git.exe"
+set cwd=%CD%
 IF NOT EXIST %mozilla_path% mkdir %mozilla_path%
+IF NOT EXIST %mozilla_path%\heka cd %mozilla_path% && %git% clone https://github.com/mozilla-services/heka.git && cd heka && %git% submodule update --init --recursive
+cd  %cwd%
 IF NOT EXIST release mkdir release
 cd release
 cmake .. -G"MinGW Makefiles"
